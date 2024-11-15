9e4a6d7904f10013c3e3722a1adb2c62d2ff1504
SamAld06 <samgaldred@gmail.com>
2024-11-15 16:10:36 +0000

    completed single page view task

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { PagesList } from './PagesList'
+
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api'
 
 export const App = () => {
   const [pages, setPages] = useState([])
+  const [currentPage, setCurrentPage] = useState(null)
+
+  async function fetchArticle (slug) {
+    const response = await fetch(`${apiURL}/wiki/${slug}`)
+    const articleData = await response.json()
+    setCurrentPage(articleData)
+  }
+
+  function goHome() {
+    setCurrentPage(null)
+  }
+
+  function createPage() {
+    
+  }
 
   useEffect(() => {
     async function fetchPages () {
@@ -21,11 +36,32 @@ export const App = () => {
     fetchPages()
   }, [])
 
+  if (!currentPage) {
+    return (
+      <main>
+        <h1>WikiVerse</h1>
+        <h2>An interesting 📚</h2>
+        <ul>{pages.map((page) => (
+          <li key = {page.id}>
+            <button onClick={() => fetchArticle(page.slug)}>{page.title}</button>
+          </li>
+        ))}</ul>
+        <button onClick={createPage}>Create Page</button>
+      </main>
+    )
+  }
   return (
-		<main>
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
-		</main>
+    <>
+      <h1>{currentPage.title}</h1>
+      <p><b>Author: </b> {currentPage.author.name}</p>
+      <p><b>Published: </b> {new Date(currentPage.createdAt).toLocaleDateString()}</p>
+      <p>{currentPage.content}</p>
+      <p><b>Tags: </b></p>
+      <ul>{currentPage.tags.map((tag) => (
+        <li key={tag.id}>{tag.name}</li>
+      ))}</ul>
+      <button onClick={goHome}>Back to Wiki List</button>
+    </>
   )
 }
+
