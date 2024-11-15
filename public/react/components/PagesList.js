4ba27e60658ddc502296d54437bc2883905c5573
SamAld06 <samgaldred@gmail.com>
2024-11-15 10:42:17 +0000

    initial commit with buttons added

--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Page } from './Page'
 
 export const PagesList = ({ pages }) => {
+	const [page, setPage] = useState("")
+
+	const handleClick = async () => {
+	const res = await fetch("/wiki/:slug")
+	const data = await res.json()
+	setPage(data)
+	
+  }
   return <>
 		{
 			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
+				return <button onClick = {handleClick}><Page page={page} key={idx} /></button>
 			})
 		}
 	</>
